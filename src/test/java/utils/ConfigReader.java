d6e7df310b67e886f4e0bdcc1bda80f0d26c5488
CodeAutomate <t.merker@simplytest.de>
2025-07-11 16:24:56 +0200

    Fixed config file

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,15 +1,18 @@
 package utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
     private static final Properties props = new Properties();
 
     static {
-        try {
-            props.load(new FileInputStream("config.properties"));
+        try (InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                throw new RuntimeException("config.properties konnte nicht geladen werden");
+            }
+            props.load(input);
         } catch (IOException e) {
             throw new RuntimeException("config.properties konnte nicht geladen werden", e);
         }
